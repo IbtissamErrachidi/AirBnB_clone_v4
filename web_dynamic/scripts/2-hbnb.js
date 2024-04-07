50a06f244d53dfee2876f6169ffc9de82d154171
malhaouit <abdelmounaim.abdel@gmail.com>
2024-04-07 08:15:25 -0700

    AirBnB_clone_v4

---
+++ web_dynamic/scripts/2-hbnb.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  const apiStatus = $('#api_status');
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+    if (data.status === 'OK') {
+      apiStatus.addClass('available');
+    } else {
+      apiStatus.removeClass('available');
+    }
+  }).fail(function () {
+    apiStatus.removeClass('available');
+  });
+});
